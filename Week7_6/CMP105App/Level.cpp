80f3bfe07a82e81f70c418de061a50d605c5cfc1
FMGD <55237715+FMGD@users.noreply.github.com>
2020-03-05 12:42:24 +0000

    Lab Done
    All bugs solved

--- Week7_6/CMP105App/Level.cpp
+++ Week7_6/CMP105App/Level.cpp
@@ -23,16 +23,21 @@ void Level::handleInput(float dt)
 // Update game objects
 void Level::update(float dt)
 {
-	for(int i = 0; i <= 6; i++)
-		bits[i].update(dt);
+	//for(int i = 0; i < sizeof(bits)/sizeof(bits[0]); i++)
+		//bits[i].update(dt);
+	for (Bit &bit : bits)
+		bit.update(dt);
 }
 
 // Render level
 void Level::render()
 {
 	beginDraw();
-	for (int i = 0; i <= sizeof(bits)/sizeof(bits[0]); i++)
-		window->draw(bits[i]);
+	//for (int i = 0; i < sizeof(bits)/sizeof(bits[0]); i++)
+		//window->draw(bits[i]);
+	for (Bit& bit : bits)
+		window->draw(bit);
+
 	endDraw();
 }
 
